272836678d105146c8d3e85432e1abd6aa0ea851
Andy Gerhard <andy@invoku.net>
2021-10-30 22:44:25 +0200

    removed unnecessary function

--- js-random-size/script.js
+++ js-random-size/script.js
@@ -3,20 +3,16 @@ let output = document.getElementById('output'),
     heightBox = document.getElementById('height'),
     borderBox = document.getElementById('border');
 
-function showDiv() {
-    let width = Math.floor(Math.random() * 500) + 100,
-        height = Math.floor(Math.random() * 300) + 100,
-        border = Math.floor(Math.random() * 30);
+let width = Math.floor(Math.random() * 500) + 100,
+    height = Math.floor(Math.random() * 300) + 100,
+    border = Math.floor(Math.random() * 30);
 
-    widthBox.innerHTML = width;
-    heightBox.innerHTML = height;
-    borderBox.innerHTML = border;
+widthBox.innerHTML = width;
+heightBox.innerHTML = height;
+borderBox.innerHTML = border;
 
-    output.innerHTML = `<div style="
-    width: ${width}px;
-    height: ${height}px;
-    border-width: ${border}px;
-    "></div>`;
-}
-
-showDiv();
+output.innerHTML = `<div style="
+width: ${width}px;
+height: ${height}px;
+border-width: ${border}px;
+"></div>`;
