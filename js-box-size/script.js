b9d079df6324b23108306caecc4af2dc46b56463
Andy Gerhard <andy@invoku.net>
2021-10-30 20:48:42 +0200

    added width and height display

--- js-box-size/script.js
+++ js-box-size/script.js
@@ -1,15 +1,19 @@
 let output = document.getElementById('output'),
     widthInput = document.getElementById('width-input'),
     heightInput = document.getElementById('height-input'),
-    area = document.getElementById('area'),
-    circumference = document.getElementById('circumference');
+    widthBox = document.getElementById('width'),
+    heightBox = document.getElementById('height'),
+    areaBox = document.getElementById('area'),
+    circumferenceBox = document.getElementById('circumference');
 
 function showDiv() {
     let width = parseInt(widthInput.value),
         height = parseInt(heightInput.value);
 
-    area.innerHTML = width * height;
-    circumference.innerHTML = (width * 2) + (height * 2);
+    widthBox.innerHTML = width;
+    heightBox.innerHTML = height;
+    areaBox.innerHTML = width * height;
+    circumferenceBox.innerHTML = (width * 2) + (height * 2);
 
     output.innerHTML = `<div style="
     width: ${width}px;
