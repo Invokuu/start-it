c423929c431fb28f5af98b4aa516f1cd34c8a57a
Andy Gerhard <andy@invoku.net>
2021-11-22 00:28:08 +0100

    js-multiplication minor fixes

--- js-multiplication/script.js
+++ js-multiplication/script.js
@@ -1,13 +1,9 @@
 const matrixView = document.getElementById('matrix');
 
-let columnCount = 5, matrix = [
-    1, 2, 3, 4, 5,
-    2, 4, 6, 8, 10,
-    3, 6, 9, 12, 15,
-    4, 8, 12, 16, 20,
-    5, 10, 15, 20, 25
-];
+// Model
+let columnCount = 5, matrix;
 
+// View
 function render() {
     let elements = [];
     for (let i = 0; i < matrix.length; i++) {
@@ -17,8 +13,7 @@ function render() {
     matrixView.style.gridTemplate = `repeat(${columnCount}, 50px) / repeat(${columnCount}, 50px)`;
 }
 
-render();
-
+// Controller
 function updateColumns(value) {
     columnCount = value;
     matrix = [];
@@ -29,3 +24,6 @@ function updateColumns(value) {
     }
     render();
 }
+
+// Initial update
+updateColumns(columnCount);
